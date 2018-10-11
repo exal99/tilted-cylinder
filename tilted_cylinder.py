ebda10dffeebf2b019654e6a4b5fe8a776effec7
Alexander Simko <asimko94@gmail.com>
2018-10-11 17:01:55 +0200

    Added marker for the cases and changed axis

--- tilted_cylinder.py
+++ tilted_cylinder.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import functools
 import numpy as np
 
+CASE = -1
+
 def debug(func):
     def wrap(*args, **kwargs):
         print(*args)
@@ -33,13 +35,13 @@ def area(length, radius, height, angle, x):
     b = (height/sin(angle) - (x+radius)/(tan(angle)))
     return a * b 
 
-a = False
-b = False
-c = False
-d = False
+def draw_case_line(max_volume, x, case):
+    plt.plot((x,x), (0,max_volume), "--")
+    plt.text(x + 0.1, 0.8*max_volume, f"Fall {case}")
 
 def volume(length, radius, angle, height):
-    global a,b,c,d
+    global CASE
+    max_volume = radius ** 2 * pi * length
     bars=1000
     area_func = functools.partial(area, length, radius, height, angle)
     if length*sin(angle) < height < 2*radius*cos(angle):
@@ -50,17 +52,21 @@ def volume(length, radius, angle, height):
         v1 = integrate(lambda x : 2 * sqrt(radius**2 - x**2) * length,
                        bars_1, -radius, x1)
         v2 = integrate(area_func, bars_2, x1, x2)
-        if not a:
-            plt.plot((height,), (v1+v2,), "bo")
-            a = True
+
+        if CASE != 0:
+            draw_case_line(max_volume, height, 3)
+            CASE = 0
+
         return v1 + v2
     if 0 <= height < 2*radius * cos(angle):
         x1 = height/cos(angle) - radius
         bars_1 = round(bars/(2*radius) * (x1 + radius))
         v=integrate(area_func, bars_1, -radius, x1)
-        if not b:
-            plt.plot((height,), (v,), "bo")
-            b = True
+
+        if CASE != 1:
+            draw_case_line(max_volume, height, 1)
+            CASE = 1
+
         return v
     if length*sin(angle) < height <= length*sin(angle) + 2*radius*cos(angle):
         x1 = height/cos(angle) - length * tan(angle) - radius
@@ -70,15 +76,18 @@ def volume(length, radius, angle, height):
                        bars_1, -radius, x1)
         v2 = integrate(area_func, bars_2, x1, radius)
 
-        if not c:
-            plt.plot((height,), (v1+v2,), "bo")
-            c = True
+        if CASE != 2:
+            draw_case_line(max_volume, height, 2)
+            CASE = 2
+
         return v1 + v2
     else:
         v=integrate(area_func, bars, -radius, radius)
-        if not d:
-            plt.plot((height,), (v,), "bo")
-            d = True
+
+        if CASE != 3:
+            draw_case_line(max_volume, height, 4)
+            CASE = 3
+
         return v
 
 def gen_points(length, radius, angle):
@@ -87,19 +96,28 @@ def gen_points(length, radius, angle):
     num_points = 1000
     return [(x, vol(x)) for x in np.linspace(0, too, num_points)]
 
+fig, ax = plt.subplots()
+#ax = fig.add_subplot(1,1,1)
+
+ax.spines['left'].set_position(('data', 0.0))
+ax.spines['bottom'].set_position(('data', 0.0))
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+#plt.xticks([2,4,6,8,10])
+#plt.yticks([50,100,150,200])
 
 points = gen_points(8,3,pi/4)
 x1, y1 = [coord[0] for coord in points], [coord[1] for coord in points]
 
-points2 = gen_points(8,3,pi/6)
-x2, y2 = [coord[0] for coord in points2], [coord[1] for coord in points2]
+#points2 = gen_points(8,3,pi/6)
+#x2, y2 = [coord[0] for coord in points2], [coord[1] for coord in points2]
 
-#x1 = 8/cos(pi/4) - 8 * tan(pi/4) - 3
-#area_func = functools.partial(area, 8, 3, 8*sin(pi/4) + 2*3*cos(pi/4), pi/4)
-#print(integrate(area_func, 1000, -3, x1) + integrate(area_func, 1000, x1, 3))
-#print(integrate(lambda x : 2*sqrt(9-x**2) * 8,1000, -3, 3))
-#print(volume(8,3,pi/4, 8*sin(pi/4) + 2*3*cos(pi/4)))
+plt.setp(ax.get_yticklabels()[1], visible=False)
+plt.setp(ax.get_xticklabels()[1], visible=False)
 
-plt.plot(x1,y1, "k", x2, y2, "b")
-print(x1[-1], y1[-1])
+plt.plot(x1,y1, "k") #, x2, y2, "b")
+
+
+
 plt.show()
